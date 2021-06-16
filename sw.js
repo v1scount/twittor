d72d8fa8dfe7ddd449d3694c33f6d05951cbb80f
v1scount <rodrigovellido1@gmail.com>
2021-06-16 04:12:24 -0300

    Tercer commit

--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@
 importScripts('js/sw-utils.js');
 
 const STATIC_CACHE = 'static-v2';
-const DYNAMIC_CACHE = 'dynamic-v1';
+const DYNAMIC_CACHE = 'dynamic-v2';
 const INMUTABLE_CACHE = 'inmutable-v1';
 
 const APP_SHELL = [
@@ -45,6 +45,9 @@ self.addEventListener('activate', e => {
       if(key !== STATIC_CACHE && key.includes('static')) {
         return caches.delete(key);
       }
+      if (  key !== DYNAMIC_CACHE && key.includes('dynamic') ) {
+        return caches.delete(key);
+    }
     });
   });
   e.waitUntil(respuesta)
